1241bb738214a087626353ea38acc3a5649678cc
Julian <julianperez.developer@gmail.com>
2025-02-24 14:32:53 -0500

    fix: remove envs

--- src/api/cointMarketCap.js
+++ src/api/cointMarketCap.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const API_KEY = '635b4d8f-dc55-4eeb-83f6-98d2e195e904'; // TODO: CHANGE IT FOR PROD
 const TEST_API_KEY = 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c';
-const CMC_URL = `https://${import.meta.env.VITE_ENV === 'prod' ? 'pro' : 'sandbox'}-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
+const CMC_URL = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
 const HEADERS = {
   'X-CMC_PRO_API_KEY':
-    import.meta.env.VITE_ENV === 'prod' ? API_KEY : TEST_API_KEY,
+  API_KEY,
   Accept: 'application/json',
 };
 
